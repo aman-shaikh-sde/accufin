7c42a08320b688469582df25c6bb8a2880f285e4
Aman Shaikh <amanshaikh05050@gmail.com>
2026-03-07 06:02:53 +0530

    logs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,18 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }; 
+const nextAuthHandler = NextAuth(authOptions);
 
+export const GET = async (req: Request, ctx: any) => {
+  console.log("nextauth GET start", req.url);
+  const res = await nextAuthHandler(req as any, ctx as any);
+  console.log("nextauth GET done", res.status);
+  return res;
+};
 
+export const POST = async (req: Request, ctx: any) => {
+  console.log("nextauth POST start", req.url);
+  const res = await nextAuthHandler(req as any, ctx as any);
+  console.log("nextauth POST done", res.status);
+  return res;
+};
